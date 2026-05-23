cdb18f69f5d33afefe6c32becb11450f8f2a1df9
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2026-05-23 21:18:45 +0100

    changed the foreground color

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,8 @@ module.exports = {
     },
     colors: {
       background: "#FFFCF6",
-      foreground: "#2A1B04",
+      // foreground: "#2A1B04",
+      foreground: "#1B1B1B",
       accent: "#F7F4ED",
       transparent: "#00000000"
     },
