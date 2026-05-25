efced47148f57025cf17cd88496ebd95188d1ee5
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2026-05-25 14:38:40 +0100

    adjusted gap between grid in event details page

--- components/pages/events/title/EventDetail.tsx
+++ components/pages/events/title/EventDetail.tsx
@@ -25,7 +25,7 @@ function EventDetail({data, title}: {readonly data: EventsQueryResult; readonly 
     ?.dropdown?.find((event) => pathname.includes(event.link))
 
   return (
-    <InfoContainer isMarginTop className="relative flex flex-col items-center md:items-baseline md:flex-row gap-5">
+    <InfoContainer isMarginTop className="relative flex flex-col items-center md:items-baseline md:flex-row gap-3 md:gap-5">
       <div className="md:self-start md:sticky md:top-28 flex-1 bg-background">
         <MainHeader
           title={event?.title ?? ''}
